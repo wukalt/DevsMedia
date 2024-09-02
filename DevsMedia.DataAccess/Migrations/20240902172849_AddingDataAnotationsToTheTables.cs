8a8eb4a9d480f333c7d4c5b1e529b683091e470f
Mehdi Mohseni <tty.mohseni@gmail.com>
2024-09-02 21:00:21 +0330

    add data anotations to the models and migration

---
+++ DevsMedia.DataAccess/Migrations/20240902172849_AddingDataAnotationsToTheTables.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevsMedia.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingDataAnotationsToTheTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Users",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Family",
+                table: "Users",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Bio",
+                table: "Users",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Admins",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Family",
+                table: "Admins",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Bio",
+                table: "Admins",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(25)",
+                oldMaxLength: 25);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Family",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(25)",
+                oldMaxLength: 25,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Bio",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Admins",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(25)",
+                oldMaxLength: 25);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Family",
+                table: "Admins",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(25)",
+                oldMaxLength: 25,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Bio",
+                table: "Admins",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+    }
+}
--- DevsMedia.Models/AbstractClasses/BaseContent.cs
+++ DevsMedia.Models/AbstractClasses/BaseContent.cs
@@ -1,10 +1,22 @@
-﻿namespace DevsMedia.Models.AbstractClasses
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace DevsMedia.Models.AbstractClasses
 {
     public abstract class BaseContent : BaseModel
     {
+        [DisplayName("عنوان")]
+        [Length(5, 50, ErrorMessage = "عنوان نباید از 5 کاراکتر کمتر و از 50 کاراکتر بیشتر باشد")]
         public string Title { get; set; } = null!;
+
+        [DisplayName("خلاصه")]
+        [Length(20, 200, ErrorMessage = "خلاصه نباید از 20 کاراکتر کمتر و از 200 کاراکتر بیشتر باشد")]
         public string ShortDescription { get; set; } = null!;
+
+        [DisplayName("کاور")]
         public string? CoverImage { get; set; }
+
+        [DisplayName("تاریخ انتشار")]
         public DateTime ReleaseTime { get; set; } = DateTime.Now;
     }
 }
