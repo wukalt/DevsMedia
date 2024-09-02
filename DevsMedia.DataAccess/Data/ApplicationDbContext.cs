e76a56f44e454eed1892e051686b19660ecd69ef
Mehdi Mohseni <tty.mohseni@gmail.com>
2024-09-03 00:11:07 +0330

    some test data, seed and migration

--- DevsMedia.DataAccess/Data/ApplicationDbContext.cs
+++ DevsMedia.DataAccess/Data/ApplicationDbContext.cs
@@ -7,12 +7,20 @@ namespace DevsMedia.DataAccess.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-            
+
         }
 
         public DbSet<User> Users { get; set; }
         public DbSet<Admin> Admins { get; set; }
         public DbSet<Blog> Blogs { get; set; }
         public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().HasData(
+                new Book() { Id = 1, Title = "C# 12 and .NET 8 – Modern Cross-Platform Development Fundamentals Eighth Edition", ShortDescription = ".NET8  آخرین نسخه اصلی از فریم ورک دات نت است، که پس از انتشار نسخه‌های قبلی، این وعده را به کاربران خود داده است که مجموعه‌ای از ویژگی‌های جدید، پیشرفت‌ها و قابلیت‌های جدید را برای توانمند‌سازی بیشتر توسعه‌دهندگان و ایجاد توسعه برنامه‌های کاربردی و لذت بخش به ارمغان بیاورد", CoverImage = "https://content.packt.com/_/image/original/B19586/cover_image_large.jpg" },
+                new Book() { Id = 2 , Title = "Cyber", ShortDescription = ".NET8  آخرین نسخه اصلی از فریم ورک دات نت است، که پس از انتشار نسخه‌های قبلی، این وعده را به کاربران خود داده است که مجموعه‌ای از ویژگی‌های جدید، پیشرفت‌ها و قابلیت‌های جدید را برای توانمند‌سازی بیشتر توسعه‌دهندگان و ایجاد توسعه برنامه‌های کاربردی و لذت بخش به ارمغان بیاورد", CoverImage = "https://content.packt.com/_/image/original/B02765/cover_image_large.jpg" }
+                );
+            }
+        }
     }
-}
