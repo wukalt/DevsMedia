285d58c8da076dd4e4737085bcf3c20bbadcdbb5
Mehdi Mohseni <tty.mohseni@gmail.com>
2024-09-03 00:09:59 +0330

    add controllers and basic views

---
+++ DevsMedia.Web/Controllers/CommunityController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevsMedia.Web.Controllers
+{
+    public class CommunityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
