37fb1c887f61eda5281f57fac0542f37766ce35a
Mehdi Mohseni <tty.mohseni@gmail.com>
2024-09-02 21:08:47 +0330

    add date time convertor utility

---
+++ DevsMedia.Utility/Convertors/Datetime.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace DevsMedia.Utility.Convertors
+{
+    public static class Datetime
+    {
+        public static DateTime ConvertToShamsi(this DateTime dateTime)
+        {
+            PersianCalendar persianCalendar = new PersianCalendar();
+            
+            int day = persianCalendar.GetDayOfMonth(dateTime);
+            int month = persianCalendar.GetMonth(dateTime);
+            int year = persianCalendar.GetYear(dateTime);
+
+            return new DateTime(year, month, day);
+        }
+    }
+}
