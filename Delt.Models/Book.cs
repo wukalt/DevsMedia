c4dc560fb4e7e20e9fcac03f1cbd9b8261dca373
Mehdi Mohseni <tty.mohseni@gmail.com>
2024-09-05 23:57:58 +0330

    Update Book Model

--- Delt.Models/Book.cs
+++ Delt.Models/Book.cs
@@ -1,9 +1,16 @@
 ﻿using Delt.Models.AbstractClasses;
+using System.ComponentModel.DataAnnotations;
 
 namespace Delt.Models
 {
     public class Book : BaseContent
     {
+        [Required(ErrorMessage = "نام نویسنده اجباری است.")]
+        [MaxLength(100, ErrorMessage = "نام نویسنده نباید از 100 کاراکتر بیشتر باشد.")]
+        public string Author { get; set; } = null!;
 
+        [Required(ErrorMessage = "نام منتشر اجباری است.")]
+        [MaxLength(100, ErrorMessage = "نام منتشر نباید از 100 کاراکتر بیشتر باشد.")]
+        public string Publisher { get; set; } = null!;
     }
 }
