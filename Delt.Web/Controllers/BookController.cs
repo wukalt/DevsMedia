4ca70d857a075df0963386aa8226ae7243fe9f9a
Mehdi Mohseni <tty.mohseni@gmail.com>
2024-09-06 03:13:58 +0330

    add basic page for Book Model

--- Delt.Web/Controllers/BookController.cs
+++ Delt.Web/Controllers/BookController.cs
@@ -1,7 +1,9 @@
 ﻿using Delt.DataAccess.Interfaces;
 using Delt.Models;
 using Delt.Utility.Checkers;
+using Microsoft.AspNetCore.Http.Metadata;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 namespace Delt.Web.Controllers
 {
@@ -31,6 +33,80 @@ namespace Delt.Web.Controllers
                 return NotFound();
 
             return View(book);
+        }
+
+        public async Task<IActionResult> Manage()
+        {
+            IEnumerable<Book> books = await _context.Book.GetAllAsync();
+            return View(books);
+        }
+
+        public async Task<IActionResult> Update(int? id)
+        {
+            if (id.IsNullOrZero())
+                return NotFound();
+
+            Book book = await _context.Book.GetAsync(book => book.Id == id);
+
+            if (book.IsNull())
+                return NotFound();
+
+            return View(book);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                await _context.Book.Update(book);
+                await _context.Commit();
+                TempData["success"] = "کتاب با موفقیت ویرایش شد.";
+                return RedirectToAction("Manage");
+            }
+            else
+            {
+                TempData["error"] = "در ویرایش کتاب مشکلی پیش آمد.";
+            }
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                await _context.Book.Add(book);
+                await _context.Commit();
+                TempData["success"] = "کتاب با موفقیت افزوده شد.";
+                return RedirectToAction("Manage");
+            }
+            TempData["error"] = "در افزودن کتاب مشکلی پیش آمده.";
+            return View();
+        }
+
+        public async Task<IActionResult> Remove(int? id)
+        {
+            if (id.IsNullOrZero())
+                return NotFound();
+
+            Book book = await _context.Book.GetAsync(book => book.Id == id);
+
+            if (book.IsNull())
+                return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                await _context.Book.Remove(book);
+                await _context.Commit();
+                TempData["success"] = "کتاب با موفقیت حذف شد.";
+            }
+            return Json(new { success = true });
         }
     }
 }
