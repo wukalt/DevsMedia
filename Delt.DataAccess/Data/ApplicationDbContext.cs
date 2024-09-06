fc16b0375804bfa84d50ae8f0ba208ceedc49ba2
Mehdi Mohseni <tty.mohseni@gmail.com>
2024-09-06 13:41:27 +0330

    remove model builder and test data

--- Delt.DataAccess/Data/ApplicationDbContext.cs
+++ Delt.DataAccess/Data/ApplicationDbContext.cs
@@ -17,15 +17,7 @@ namespace Delt.DataAccess.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Book>().HasData(
-                new Book() { Id = 1, Title = "C# 12 and .NET 8 – Modern Cross-Platform Development Fundamentals Eighth Edition", ShortDescription = ".NET8  آخرین نسخه اصلی از فریم ورک دات نت است، که پس از انتشار نسخه‌های قبلی، این وعده را به کاربران خود داده است که مجموعه‌ای از ویژگی‌های جدید، پیشرفت‌ها و قابلیت‌های جدید را برای توانمند‌سازی بیشتر توسعه‌دهندگان و ایجاد توسعه برنامه‌های کاربردی و لذت بخش به ارمغان بیاورد", CoverImage = "https://content.packt.com/_/image/original/B19586/cover_image_large.jpg", Author = "Mark J Price", Publisher ="packt", ReleaseTime = new DateTime(2024/01/01), DownloadLink="www.google.com" },
-                new Book() { Id = 2 , Title = "Cyber", ShortDescription = ".NET8  آخرین نسخه اصلی از فریم ورک دات نت است، که پس از انتشار نسخه‌های قبلی، این وعده را به کاربران خود داده است که مجموعه‌ای از ویژگی‌های جدید، پیشرفت‌ها و قابلیت‌های جدید را برای توانمند‌سازی بیشتر توسعه‌دهندگان و ایجاد توسعه برنامه‌های کاربردی و لذت بخش به ارمغان بیاورد", CoverImage = "https://content.packt.com/_/image/original/B02765/cover_image_large.jpg", Author = "Mark J Price", Publisher = "packt", ReleaseTime = new DateTime(2024 / 01 / 01), DownloadLink = "www.google.com" }
-                );
-
-            modelBuilder.Entity<Blog>().HasData(
-                new Blog() {Id =1, Title = "چگونه در سی شارپ SOLID را پیاده سازی کنیم", BlogText = "SOLID که متشکل از 5 اصل است در برنامه نویسی  بهتر کمک میکمند", ShortDescription = "SOLID که متشکل از 5 اصل است در برنامه نویسی  بهتر کمک میکمند", CoverImage="null for now" },
-                new Blog() {Id =2, Title = "Asyncronous را پیاده سازی کنیم", BlogText = "async await که متشک  بهتر کمک میکمند", ShortDescription = "SOLID که متشکل از 5 اصل است در برنامه نویسی  بهتر کمک میکمند", CoverImage="null for now" }
-                );
+            
             }
         }
     }
