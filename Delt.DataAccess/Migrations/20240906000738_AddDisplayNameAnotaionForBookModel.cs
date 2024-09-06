71ac4edea5e4cf14e34edcded00e722c1a0ef6d8
Mehdi Mohseni <tty.mohseni@gmail.com>
2024-09-06 03:38:32 +0330

    add DisplayName Anotation for Book Model

---
+++ Delt.DataAccess/Migrations/20240906000738_AddDisplayNameAnotaionForBookModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Delt.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDisplayNameAnotaionForBookModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ReleaseTime",
+                value: new DateTime(2024, 9, 6, 3, 37, 36, 692, DateTimeKind.Local).AddTicks(4699));
+
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ReleaseTime",
+                value: new DateTime(2024, 9, 6, 3, 37, 36, 692, DateTimeKind.Local).AddTicks(4701));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ReleaseTime",
+                value: new DateTime(2024, 9, 6, 0, 6, 6, 949, DateTimeKind.Local).AddTicks(612));
+
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ReleaseTime",
+                value: new DateTime(2024, 9, 6, 0, 6, 6, 949, DateTimeKind.Local).AddTicks(614));
+        }
+    }
+}
--- Delt.Models/Book.cs
+++ Delt.Models/Book.cs
@@ -1,4 +1,5 @@
 ﻿using Delt.Models.AbstractClasses;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace Delt.Models
@@ -7,12 +8,15 @@ namespace Delt.Models
     {
         [Required(ErrorMessage = "نام نویسنده اجباری است.")]
         [MaxLength(100, ErrorMessage = "نام نویسنده نباید از 100 کاراکتر بیشتر باشد.")]
+        [DisplayName("نویسنده")]
         public string Author { get; set; } = null!;
 
         [Required(ErrorMessage = "نام منتشر اجباری است.")]
         [MaxLength(100, ErrorMessage = "نام منتشر نباید از 100 کاراکتر بیشتر باشد.")]
+        [DisplayName("ناشر")]
         public string Publisher { get; set; } = null!;
 
+        [DisplayName("لینک دانلود")]
         [Required(ErrorMessage = "لینک دانلود الزامی است.")]
         public string DownloadLink { get; set; } = null!;
     }
