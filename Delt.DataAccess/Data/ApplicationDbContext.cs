5260300a2686cb92b34b29bef4c07fbf2af1cef0
Mehdi Mohseni <tty.mohseni@gmail.com>
2024-09-06 15:15:18 +0330

    add Teacher table and seed some test data

--- Delt.DataAccess/Data/ApplicationDbContext.cs
+++ Delt.DataAccess/Data/ApplicationDbContext.cs
@@ -14,11 +14,18 @@ namespace Delt.DataAccess.Data
         public DbSet<Admin> Admins { get; set; }
         public DbSet<Blog> Blogs { get; set; }
         public DbSet<Book> Books { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
         public DbSet<Seminar> Seminars { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            
-            }
+            modelBuilder.Entity<Teacher>().HasData(
+                new Teacher() { Id = 1, Bio = "not to as", Experience = 7, Name = "ali", Family = ",p", ProfileImage = "null for bnow" }
+                );
+
+            modelBuilder.Entity<Seminar>().HasData(
+                new Seminar() { Id = 1, Title = "this is title", CoverImage = "null for now", Level = Models.Enums.SeminarLevel.Starter, ShortDescription = "dksjjdksjdkjskjdksjdksjdkjskdkjsd", TeacherId = 1 }
+                );
         }
     }
+}
