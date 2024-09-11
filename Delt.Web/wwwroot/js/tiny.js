1c750ea16b1ff2c4dba8632d4cb3f8ae92daf822
Mehdi Mohseni <tty.mohseni@gmail.com>
2024-09-11 03:35:43 +0330

    change tiny toolbar

--- Delt.Web/wwwroot/js/tiny.js
+++ Delt.Web/wwwroot/js/tiny.js
@@ -2,12 +2,9 @@
     selector: '#tinyEditor',
     plugins: [
         // Core editing features
-        'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'image', 'link', 'lists', 'media', 'searchreplace', 'table', 'visualblocks', 'wordcount',
-        // Your account includes a free trial of TinyMCE premium features
-        // Try the most popular premium features until Sep 20, 2024:
-        'checklist', 'mediaembed', 'casechange', 'export', 'formatpainter', 'pageembed', 'a11ychecker', 'tinymcespellchecker', 'permanentpen', 'powerpaste', 'advtable', 'advcode', 'editimage', 'advtemplate', 'ai', 'mentions', 'tinycomments', 'tableofcontents', 'footnotes', 'mergetags', 'autocorrect', 'typography', 'inlinecss', 'markdown',
+        'link',
     ],
-    toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+    toolbar: 'undo redo | fontfamily fontsize | bold italic underline | link | align | indent outdent |',
     tinycomments_mode: 'embedded',
     tinycomments_author: 'Author name',
     mergetags_list: [
